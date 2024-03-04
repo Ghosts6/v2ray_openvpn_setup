8b4ff65303fb04b74d9900ca4ed4592c6d58db1f
Kiarash <95994481+Ghosts6@users.noreply.github.com>
2024-03-04 14:54:56 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,6 +3,10 @@
 echo "Updating software..."
 sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get autoremove -y
 
+echo "Enableing ip forwarding.."
+sudo sysctl -w net.ipv4.ip_forward=1
+sudo sysctl -p
+
 echo "Installing firewall..."
 sudo apt-get install ufw
 
