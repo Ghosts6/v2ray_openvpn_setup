59b42454386fcdfd09a23a9b802c366505543b7f
Kiarash <95994481+Ghosts6@users.noreply.github.com>
2024-03-13 14:02:53 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -10,12 +10,19 @@ echo "Starting V2Ray..."
 sudo systemctl start v2ray
 
 # Start openVpn
-echo "Starting Squid vpn server..."
+echo "Starting open vpn server..."
 sudo systemctl start openvpn@server || sudo systemctl start openvpn-server@server.service
+
+# start squid
+echo " Starting Squid ..."
+sudo systemctl start squid
 
 # Enable services to start on boot
 echo "Enabling services to start on boot..."
 sudo systemctl enable v2ray
-sudo systemctl enable openvpn@server 
+sudo systemctl enable openvpn@server
+sudo systemctl enable squid 
+
+
 
 echo "Services started and enabled successfully!"
