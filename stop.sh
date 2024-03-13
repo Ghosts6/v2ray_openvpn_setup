6e4a2f8e2c12aa41607f09dd79288e4c724ad4b5
Kiarash <95994481+Ghosts6@users.noreply.github.com>
2024-03-13 14:04:13 +0000

    Update stop.sh

--- stop.sh
+++ stop.sh
@@ -4,8 +4,12 @@
 echo "Stopping V2Ray"
 sudo systemctl stop v2ray
 
-# Stop Squid Proxy
+# Stop open vpn
 echo "Stopping openvpn vpn server ..."
 sudo systemctl stop openvpn@server || sudo systemctl stop openvpn-server@server.service
+
+# Stop squid
+echo "Stopping squid ..."
+sudo systemctl stop squid
 
 echo "Services stopped successfully"
