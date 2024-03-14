00d2bb4e1ddf711379e4685a33ae1abef627663d
Kiarash <95994481+Ghosts6@users.noreply.github.com>
2024-03-14 10:50:30 +0000

    Update squid.sh

--- squid.sh
+++ squid.sh
@@ -22,12 +22,12 @@ sudo ufw allow $port
 sudo ufw reload
 sudo ufw status
 
-echo "startin service..."
+echo "starting service..."
 sudo systemctl restart squid
 sudo systemctl status squid
 
 echo "create user..."
-apt-get install apache2-utils -y
+sudo apt-get install apache2-utils -y
 sudo touch /etc/squid/passwd
 sudo chown proxy: /etc/squid/passwd
 
